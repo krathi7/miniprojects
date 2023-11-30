e4484c34a16906bf6779d019ecfa4ac5ca9d2f54
Krathi <151897447+krathi7@users.noreply.github.com>
2023-11-30 12:37:31 +0530

    Add files via upload

---
+++ minipro.py
@@ -0,0 +1,7 @@
+import random as r
+m= int(input("How many numbers do you want in your password?"))
+j= int(input("How many character do you want in your password?"))
+t= int(input("How many symbols do you want in your pasword?"))
+characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+symbols =["!","@","#","$","%","^","&","*","(",")","<",">","_","=","+","?","~","`"]
+numbers =["0","1","2","3","4","5","6","7","8","9"]
